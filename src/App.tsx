41ffd8deb3bfbfe40d0c0685d0c5b13bf71bd9fe
AlexandrIvanov <alexandr.1996@list.ru>
2023-04-25 12:42:51 +0400

    add Layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,25 @@
 import React from 'react';
 import './App.css';
-import {Link, Route, Routes} from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 import {HomePage} from './pages/HomePage';
 import {AboutPage} from './pages/AboutPage';
 import {BlogPage} from './pages/BlogPage';
 import {NotFoundPage} from './pages/NotFoundPage';
+import {Layout} from './components/Layout';
 
 function App() {
-  return (
-    <div className="App">
-      <header>
-        <Link to="/">Home</Link>
-        <Link to="/posts">Blog</Link>
-        <Link to="/about">About</Link>
-        <Link to="/notFound">Not found</Link>
-      </header>
-        <Routes>
-            <Route path='/' element={<HomePage/>}/>
-            <Route path='/posts' element={<BlogPage/>}/>
-            <Route path='/about' element={<AboutPage/>}/>
-            <Route path='*' element={<NotFoundPage/>}/>
-        </Routes>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/" element={<Layout/>}>
+                    <Route index element={<HomePage/>}/>
+                    <Route path="posts" element={<BlogPage/>}/>
+                    <Route path="about" element={<AboutPage/>}/>
+                    <Route path="*" element={<NotFoundPage/>}/>
+                </Route>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Link, Outlet} from 'react-router-dom';
+
+export const Layout = () => {
+    return (
+        <>
+            <header>
+                <Link to="/">Home</Link>
+                <Link to="/posts">Blog</Link>
+                <Link to="/about">About</Link>
+            </header>
+            <Outlet/>
+        </>
+    )
+}
